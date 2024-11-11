1fd2712f76ba3e8b96105531793c72a5de533882
Cristian Mesaros <Cristian-Ioan.MESAROS@ext.ec.europa.eu>
2024-11-11 23:15:02 +0100

    Initial

--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -7,10 +7,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Mesaros_Cristian_Lab2.Data;
 using Mesaros_Cristian_Lab2.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mesaros_Cristian_Lab2.Pages.Books
 {
-    public class CreateModel : PageModel
+    public class CreateModel : BookCategoriesPageModel
     {
         private readonly Mesaros_Cristian_Lab2.Data.Mesaros_Cristian_Lab2Context _context;
 
@@ -23,6 +24,9 @@ namespace Mesaros_Cristian_Lab2.Pages.Books
         {
             ViewData["AuthorID"] = new SelectList(_context.Set<Author>(), "ID", "FullName");
             ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
+            var book = new Book();
+            Book.BookCategories = new List<BookCategory>();
+            PopulateAssignedCategoryData(_context, book);
             return Page();
         }
 
@@ -30,17 +34,25 @@ namespace Mesaros_Cristian_Lab2.Pages.Books
         public Book Book { get; set; } = default!;
 
         // For more information, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(string[] selectedCategories)
         {
-            if (!ModelState.IsValid)
+            var newBook = new Book();
+            if (selectedCategories != null)
             {
-                return Page();
+                newBook.BookCategories = new List<BookCategory>();
+                foreach (var cat in selectedCategories)
+                {
+                    var catToAdd = new BookCategory
+                    {
+                        CategoryID = int.Parse(cat)
+                    };
+                    newBook.BookCategories.Add(catToAdd);
+                }
             }
-
+            Book.BookCategories = newBook.BookCategories;
             _context.Book.Add(Book);
             await _context.SaveChangesAsync();
-
             return RedirectToPage("./Index");
         }
     }
-}
+    }
