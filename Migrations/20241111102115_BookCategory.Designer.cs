bc1456c44d9a16ce915e699d0fbed7a4be6e2bfa
Cristian Mesaros <Cristian-Ioan.MESAROS@ext.ec.europa.eu>
2024-11-11 11:51:40 +0100

    Save status punct 13

--- Migrations/20241111102115_BookCategory.Designer.cs
+++ Migrations/20241111102115_BookCategory.Designer.cs
@@ -4,6 +4,7 @@ using Mesaros_Cristian_Lab2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Mesaros_Cristian_Lab2.Migrations
 {
     [DbContext(typeof(Mesaros_Cristian_Lab2Context))]
-    partial class Mesaros_Cristian_Lab2ContextModelSnapshot : ModelSnapshot
+    [Migration("20241111102115_BookCategory")]
+    partial class BookCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Book.cs
+++ Models/Book.cs
@@ -18,5 +18,6 @@ namespace Mesaros_Cristian_Lab2.Models
         public DateTime PublishingDate { get; set; }
         public int? PublisherID { get; set; }
         public Publisher? Publisher { get; set; }
+        public ICollection<BookCategory>? BookCategories { get; set; }
     }
 }
