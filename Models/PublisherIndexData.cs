bf1f6d078dede630776039f325ed6d4978cbed69
Cristian Mesaros <Cristian-Ioan.MESAROS@ext.ec.europa.eu>
2024-11-17 08:40:17 +0100

    Inaite de laborator

---
+++ Models/PublisherIndexData.cs
@@ -0,0 +1,9 @@
+﻿using Mesaros_Cristian_Lab2.Models;
+namespace Mesaros_Cristian_Lab2.Models
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
