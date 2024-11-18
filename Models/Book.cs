a6495aa85e9b0c2dce89acc469cbc51bb74a16ec
Cristian Mesaros <Cristian-Ioan.MESAROS@ext.ec.europa.eu>
2024-11-18 12:09:03 +0100

    Laborator5 inaite de Identity

--- Models/Book.cs
+++ Models/Book.cs
@@ -18,6 +18,7 @@ namespace Mesaros_Cristian_Lab2.Models
         public DateTime PublishingDate { get; set; }
         public int? PublisherID { get; set; }
         public Publisher? Publisher { get; set; }
+        public ICollection<Borrowing>? Borrowings { get; set; }
         public ICollection<BookCategory>? BookCategories { get; set; }
     }
 }
---
+++ Models/Member.cs
@@ -0,0 +1,25 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
+
+namespace Mesaros_Cristian_Lab2.Models
+{
+    public class Member
+    {
+        public int ID { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Adress { get; set; }
+        public string Email { get; set; }
+        public string? Phone { get; set; }
+        [Display(Name = "Full Name")]
+        public string? FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+        public ICollection<Borrowing>? Borrowings { get; set; }
+    }
+}
