861159e51c201ac8640900447497c0f6c9cd2aec
Cristian Mesaros <Cristian-Ioan.MESAROS@ext.ec.europa.eu>
2024-12-13 13:45:52 +0100

    fixed some erroer

--- Models/Book.cs
+++ Models/Book.cs
@@ -10,10 +10,10 @@ namespace Mesaros_Cristian_Lab2.Models
 
         [Display(Name = "Book Title")]
         public string Title { get; set; }
-        [Column(TypeName = "decimal(6, 2)") [Range(0.01, 500)]
         public int? AuthorID { get; set; }
         public Author? Author { get; set; }
         [Column(TypeName = "decimal(6, 2)")]
+        [Range(0.01, 500, ErrorMessage = "Price must be between 0.01 and 500.")]
         public decimal Price { get; set; }
         [DataType(DataType.Date)] 
         public DateTime PublishingDate { get; set; }
