114f6f38e94ea338a9b86ad75aa64c64ea1bf750
Cristian Mesaros <Cristian-Ioan.MESAROS@ext.ec.europa.eu>
2024-12-13 14:11:47 +0100

    Laborator6  sarcina laborator 2

--- Models/Book.cs
+++ Models/Book.cs
@@ -7,7 +7,8 @@ namespace Mesaros_Cristian_Lab2.Models
     public class Book
     {
         public int ID { get; set; }
-
+        [Required(ErrorMessage = "Este necesar campul cu nume sa fie complectat.")]
+        [StringLength(150, MinimumLength = 3, ErrorMessage = "Titlul trebuie sa contina intre 3 si 150 caractere.")]
         [Display(Name = "Book Title")]
         public string Title { get; set; }
         public int? AuthorID { get; set; }
@@ -15,7 +16,8 @@ namespace Mesaros_Cristian_Lab2.Models
         [Column(TypeName = "decimal(6, 2)")]
         [Range(0.01, 500, ErrorMessage = "Price must be between 0.01 and 500.")]
         public decimal Price { get; set; }
-        [DataType(DataType.Date)] 
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime PublishingDate { get; set; }
         public int? PublisherID { get; set; }
         public Publisher? Publisher { get; set; }
